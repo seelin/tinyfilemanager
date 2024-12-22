fd2fdb13465c084976fbb7d7de434ede4f74e9a0
seelin <seelin@126.com>
2024-12-23 04:32:21 +0800

    Update rindex.py

--- rindex.py
+++ rindex.py
@@ -19,7 +19,7 @@ def hello_world():
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index.php', methods=['GET', 'POST'])
 def forward():
-    url = 'http://127.0.0.1:82/hello'
+    url = 'http://127.0.0.1/'
     method = request.method
     data = request.get_data()
    
